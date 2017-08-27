bc64de55a2bd10bb18632c08d61c7defb3560358
Garrett Johnson <garrett.kjohnson@gmail.com>
2017-08-27 15:35:39 -0700

    Add example of alpha oscillation

---
+++ Assets/OscillateAlpha.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OscillateAlpha : MonoBehaviour {
+
+    private void Start()
+    {
+        GetComponent<Renderer>().sharedMaterial = new Material(GetComponent<Renderer>().sharedMaterial);
+    }
+    void Update () {
+        Material mat = GetComponent<Renderer>().sharedMaterial;
+        Color col = mat.color;
+        col.a = Mathf.Sin(Time.timeSinceLevelLoad * 2) * 0.5f + 0.5f;
+        mat.color = col;
+	}
+}
